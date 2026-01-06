056ba8f0732988c66cc1dc7cdfc9ca73c06e6a8c
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-06 16:10:48 +0530

    [Prashant] add program to sort marks using Bubble sort

---
+++ Sorting/SortMarks.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Advance_Programming_DSA.Sorting
+{
+    public class SortMarks
+    {
+        public void Sort(int[] marks)
+        {
+            for(int i=0; i<marks.Length; i++)
+            {
+                for(int j=0; j<marks.Length-i-1; j++)
+                {
+                    if (marks[j] > marks[j + 1])
+                    {
+                        int temp = marks[j];
+                        marks[j] = marks[j + 1];
+                        marks[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
