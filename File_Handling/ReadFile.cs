a7126a0d47d4bc928990e9f704aaf3e3decd0c02
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-07 15:58:41 +0530

    [Prashant] add program to read data line by line from a file using StreamReader

---
+++ File_Handling/ReadFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.File_Handling
+{
+    /*
+     Problem: Write a program that uses StreamReader to read a text file line by line and print
+each line to the console.
+     */
+    internal class ReadFile
+    {
+        public void Read()
+        {
+            string filePath = "sample.txt";
+            try
+            {
+                using(StreamReader sr = new StreamReader(filePath))
+                {
+                    string line;
+                    while((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            } catch(IOException e)
+            {
+                Console.WriteLine (e.Message);
+            }
+        }
+    }
+}
