9ace3df0e23e51046769a013a4b0ab66000e296a
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-07 12:46:02 +0530

    [Prashant] add code to search target element in 2-D array using binary search

---
+++ Searching/SearchMatrix.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.Searching
+{
+    /*
+     Problem: You are given a 2D matrix where each row is sorted in ascending order. Write a
+program that performs Binary Search to find a target value in the matrix.
+     */
+    internal class SearchMatrix
+    {
+        public bool Search(int[,] arr, int target)
+        {
+            int r = 0;
+            int c = arr.GetLength(1) - 1;
+            while(r<arr.GetLength(0) && c >= 0)
+            {
+                if(arr[r, c] == target)
+                {
+                    return true;
+                }
+                else if (arr[r,c] > target)
+                {
+                    c--;
+                }
+                else
+                {
+                    r++;
+                }
+            }
+            return false;
+        }
+    }
+}
