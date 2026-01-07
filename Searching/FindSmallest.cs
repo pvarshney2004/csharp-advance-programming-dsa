f017674b2e614e68783e4ef48799050a3e84de02
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-07 12:45:10 +0530

    [Prashant] add code to find smallest element in rotated sorted array using binary search

---
+++ Searching/FindSmallest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.Searching
+{
+    /*
+     Problem: You are given a rotated sorted array. Write a program that performs Binary Search
+to find the index of the smallest element in the array.
+     */
+    internal class FindSmallest
+    {
+        public int Find(int[] arr)
+        {
+            int i = 0;
+            int j = arr.Length - 1;
+            while (i < j)
+            {
+                int mid = (i + j) / 2;
+                if (arr[mid] > arr[j])
+                {
+                    i  = mid + 1;
+                }
+                else
+                {
+                    j = mid;
+                }
+            }
+            return i;
+        }
+    }
+}
