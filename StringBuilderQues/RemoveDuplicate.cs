5f1f909e06f43531895baa6d35ab9741b700fdc6
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-07 15:17:29 +0530

    [Prashant] add program to remove duplicates characters from string using StringBuilder

---
+++ StringBuilderQues/RemoveDuplicate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.StringBuilderQues
+{
+    /*
+     Problem: Write a program that uses StringBuilder to remove all duplicate characters from a
+given string while maintaining the original order.
+     */
+    internal class RemoveDuplicate
+    {
+       public void Remove(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool[] seen = new bool[256]; 
+
+            foreach (char c in str)
+            {
+                if (!seen[c])
+                {
+                    sb.Append(c);
+                    seen[c] = true;
+                }
+            }
+            Console.WriteLine($"Original String: {str} | Updated String: {sb.ToString()}");
+        }
+    }
+}
