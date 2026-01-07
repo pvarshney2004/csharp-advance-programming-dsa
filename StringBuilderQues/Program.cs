fe69ac0b32b77660df7e5e00ea242e3a2b87deb8
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-07 15:18:54 +0530

    [Prashant] add Program.cs file

---
+++ StringBuilderQues/Program.cs
@@ -0,0 +1,27 @@
+﻿using Advance_Programming_DSA.StringBuilderQues;
+using System;
+namespace Advance_Programming_DSA.StringBuidler
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Reverse obj = new Reverse();
+            //String str = "hello";
+            //obj.ReverseString(str);
+
+            //RemoveDuplicate obj2 = new RemoveDuplicate();
+            //String str = "programming";
+            //obj2.Remove(str);
+
+            //ConcatenateStrings obj3 = new ConcatenateStrings();
+            //string[] arr = { "hello", " ", "my", " ", "name", " ", "is", " ", "Prashant" };
+            //obj3.Solve(arr);
+
+            StringBuilderPerformance obj4 = new StringBuilderPerformance();
+            obj4.Compare();
+
+
+        }
+    }
+}
