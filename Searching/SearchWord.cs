04af55708093caf84c1cb674d91b25cbd4188c60
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-07 12:44:00 +0530

    [Prashant] add code for finding first sentence that contains target word in string array using Linear search

---
+++ Searching/SearchWord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.Searching
+{
+    /*
+     Problem: You are given an array of sentences. Write a program that performs Linear Search
+to find the first sentence containing a specific word.
+     */
+    internal class SearchWord
+    {
+        public int Search(string[] sentences, string word)
+        {
+            for(int i=0; i < sentences.Length; i++)
+            {
+                if (sentences[i].Contains(word))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
