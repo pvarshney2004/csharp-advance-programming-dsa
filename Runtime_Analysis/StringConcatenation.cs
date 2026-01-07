f151f1b18df834646a9d430365e69d9cc1f6ce11
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-07 22:14:23 +0530

    [Prashant] add code for comparing runtime in ms for string and stringbuilder

---
+++ Runtime_Analysis/StringConcatenation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.Runtime_Analysis
+{
+    /*
+     3. String Concatenation Performance
+Objective
+Compare the performance of string (O(N²)), StringBuilder (O(N)), and StringBuffer (O(N))
+when concatenating a million strings.
+Approach
+● Using string (Immutable, creates a new object each time)
+● Using StringBuilder (Fast, mutable, thread-unsafe)w
+     */
+    internal class StringConcatenation
+    {
+        public static void Compare(int n)
+        {
+            // Using string concatenation
+            Stopwatch sw1 = Stopwatch.StartNew();
+            string str = "";
+            for (int i = 0; i < n; i++)
+            {
+                str += "hello";
+            }
+            sw1.Stop();
+
+            // Using StringBuilder concatenation
+            Stopwatch sw2 = Stopwatch.StartNew();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append("hello");
+            }
+            sb.ToString();
+            sw2.Stop();
+
+            Console.WriteLine("String concatenation Time: " + sw1.ElapsedMilliseconds + " ms");
+            Console.WriteLine("StringBuilder concatenation Time: " + sw2.ElapsedMilliseconds + " ms");
+        }
+    }
+}
