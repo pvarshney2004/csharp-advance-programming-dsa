d20a42e2c18e8f306fceaa22afcd52ac5581096a
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-07 12:42:46 +0530

    [Prashant] add code for finding first negative number in array using Linear search

---
+++ Searching/FirstNegative.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.Searching
+{
+    /*
+     Problem: You are given an integer array. Write a program that performs Linear Search to find
+the first negative number in the array.
+     */
+    internal class FirstNegative
+    {
+        public int Find(int[] arr)
+        {
+            for(int i=0; i<arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
