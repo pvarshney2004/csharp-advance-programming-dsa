ff3cbc6e577ba2a1042a4d6b66be467a9b068711
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-07 16:01:45 +0530

    [Prashant] add Program.cs file

---
+++ File_Handling/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Advance_Programming_DSA.File_Handling
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //ReadFile obj = new ReadFile();
+            //obj.Read();
+
+            //CountWord obj2 = new CountWord();
+            //string target = "lorem";
+            //obj2.Count(target);
+            //string target2 = "is";
+            //obj2.Count(target2);
+
+            //ByteToCharStream obj3 = new ByteToCharStream();
+            //obj3.Print();
+
+            WriteUserInputToFile obj4 = new WriteUserInputToFile();
+            obj4.Write();
+        }
+    }
+}
