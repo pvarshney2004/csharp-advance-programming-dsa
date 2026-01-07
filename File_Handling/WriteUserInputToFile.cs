ff6f202d17e9a1bd1f793d60722cc78d16788618
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-07 16:01:31 +0530

    [Prashant] add program to read user data and write that data in a output file

---
+++ File_Handling/WriteUserInputToFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.File_Handling
+{
+    internal class WriteUserInputToFile
+    {
+        public void Write()
+        {
+            string filePath = "output.txt";
+
+            Console.WriteLine("Enter text that you want to write in file:");
+            string input = Console.ReadLine();
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine(input);
+                Console.WriteLine("Data written to the output file successfully.");
+            }
+        }
+    }
+}
