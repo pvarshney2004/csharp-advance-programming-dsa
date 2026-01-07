13f86991ed9dda1c22daa3c20f3d48631553a8c7
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-07 16:00:07 +0530

    [Prashant] add program to read binary data and print characters using StreamReader

---
+++ File_Handling/ByteToCharStream.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.File_Handling
+{
+    /*
+     Problem: Write a program that uses StreamReader to read binary data from a file and print
+it as characters.
+     */
+    internal class ByteToCharStream
+    {
+        public void Print()
+        {
+            string filePath = "sample.txt";
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                int ch;
+                while ((ch = reader.Read()) != -1)
+                {
+                    Console.Write((char)ch+" ");
+                }
+            }
+        }
+    }
+}
