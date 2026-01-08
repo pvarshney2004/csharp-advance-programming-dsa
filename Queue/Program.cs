74a310a7fe97c7684a9c346aee2f9c9a0a332760
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-08 15:16:15 +0530

    [Prashant] add Program.cs file

---
+++ Queue/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Advance_Programming_DSA.Queue
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //PrintQueue pq = new PrintQueue();
+            //pq.AddJob(new TextDocument());
+            //pq.AddJob(new ImageFile());
+            //pq.PrintNext();
+            //pq.PrintNext();
+            //pq.PrintNext();
+
+
+            //BookingQueue bq = new BookingQueue();
+            //bq.AddRequest(new FlightTicket());
+            //bq.AddRequest(new TrainTicket());
+            //bq.AddRequest(new BusTicket());
+            //bq.BookNext();
+            //bq.BookNext();
+            //bq.BookNext();
+            //bq.BookNext();
+
+
+            //TaskQueue tq = new TaskQueue();
+            //tq.AddTask(new UploadTask());
+            //tq.AddTask(new ReportTask());
+            //tq.AddTask(new UploadTask());
+            //tq.DispatchTask();
+            //tq.DispatchTask();
+            //tq.DispatchTask();
+            //tq.DispatchTask();
+
+
+            CallCenterQueue cq = new CallCenterQueue();
+            cq.AddRequest(new SupportCall());
+            cq.AddRequest(new SalesCall());
+            cq.AddRequest(new SupportCall());
+            cq.HandleNext();
+            cq.HandleNext();
+            cq.HandleNext();
+            cq.HandleNext();
+
+
+
+        }
+    }
+}
