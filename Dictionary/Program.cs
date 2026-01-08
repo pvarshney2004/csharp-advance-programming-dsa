f34e3344864821d3f0f0b325c920a180a1b771e7
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-08 18:16:15 +0530

    [Prashant] add Program.cs file

---
+++ Dictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Advance_Programming_DSA.Dictionary
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //InventoryManager manager = new InventoryManager();
+            //manager.Add(101, new Electronics());
+            //manager.Add(102, new Clothing());
+            //manager.Get(101).ShowDetails();
+            //manager.Get(102).ShowDetails();
+
+            //AuthSystem auth = new AuthSystem();
+            //auth.AssignRole("admin1", new Admin());
+            //auth.AssignRole("user1", new User());
+            //Console.WriteLine(auth.HasAccess("admin1", "DELETE")); 
+            //Console.WriteLine(auth.HasAccess("user1", "DELETE"));  
+            //Console.WriteLine(auth.HasAccess("user1", "READ"));  
+
+            //GradeBook g = new GradeBook();
+            //g.AddStudent("pv", new CBSE());
+            //g.AddStudent("pv2", new GPA());
+            //g.ShowGrade("pv", 91);
+            //g.ShowGrade("pv2", 89);
+
+            IURLStorage storage = new MemoryStorage();
+            storage.Save("bridge.com", "https://bridgelabz.com");
+            Console.WriteLine(storage.Get("bridge.com"));
+
+
+        }
+    }
+}
