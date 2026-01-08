1b872b4d5954898029fa0d0759dc67bcb1331a91
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-08 15:16:02 +0530

    [Prashant] add code for handling calls request in queue

---
+++ Queue/CallCenterQueue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.Queue
+{
+    /*
+     4.Call Center Management System
+Use Case: Incoming calls are queued for available agents.
+OOP Concepts:
+● Interface: CallRequest
+● Polymorphism: Calls could be SupportCall, SalesCall, etc.
+● Encapsulation: Queue handling hidden inside CallCenterManager
+     */
+    // interface
+    interface ICallRequest
+    {
+        void CallingRequest();
+    }
+
+    // subclass implementing CallingRequest method of interface
+    class SupportCall : ICallRequest
+    {
+        public void CallingRequest()
+        {
+            Console.WriteLine("Support call request received and handled.");
+        }
+    }
+    class SalesCall : ICallRequest
+    {
+        public void CallingRequest()
+        {
+            Console.WriteLine("Sales call request received and handled.");
+        }
+    }
+
+    // class CallCenterQueue that will handle all the incoming calls
+    internal class CallCenterQueue
+    {
+        Queue<ICallRequest> q = new Queue<ICallRequest>();
+        public void AddRequest(ICallRequest callRequest)
+        {
+            q.Enqueue(callRequest);
+        }
+
+        public void HandleNext()
+        {
+            if (q.Count > 0)
+            {
+                q.Dequeue().CallingRequest();
+            }
+            else
+            {
+                Console.WriteLine("No call request available.");
+            }
+        }
+    }
+}
