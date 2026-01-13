6741020258c7fee13d200334da3588cec55e9519
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-13 14:54:00 +0530

    [Prashant] add Program.cs file

---
+++ Delegates/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Advance_Programming_DSA.Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Operation del1 = Calculation.Add;
+            //int sum = del1(2, 3);
+            //Console.WriteLine("Addition: " + sum);
+            //del1 = Calculation.Substract;
+            //int sub = del1(9, 2);
+            //Console.WriteLine("Substraction: " + sub);
+            //del1 = Calculation.Multiply;
+            //int mul = del1(10, 3);
+            //Console.WriteLine("Multiplication: " + mul);
+
+            //Print del2 = Messages.WelcomeMessage;
+            //del2.Invoke("Welcome to the world!");
+            //del2 = Messages.ErrorMessage;
+            //del2.Invoke("Error 404!");
+            //del2 = Messages.SuccessMessage;
+            //del2.Invoke("Success party!");
+
+            //MyDelegate del3 = DelegateAsParameter.Greet;
+            //DelegateAsParameter.InvokeDelegate(del3);
+
+            //MyDel del4 = MulticastDelegate.MethodA;
+            //MyDel del5 = MulticastDelegate.MethodB;
+            //MyDel del6 = del4 + del5;
+            //del6.Invoke();
+
+            //Printt print = delegate (int val) {
+            //    Console.WriteLine("Inside Anonymous method. Value: {0}", val);
+            //};
+            //print(100);
+
+            //DelegateVsFuncAct.Execute();
+
+            //DelegateChaining.Execute();
+
+            //DelegateValidation.Execute();
+
+            //DelegateCallback.DownloadFile(DelegateCallback.OnDownloadCompleted);
+
+
+        }
+    }
+}
