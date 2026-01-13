731d775d1167d9fe644a0ce8625802a09d1d3210
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-13 14:53:47 +0530

    [Prashant] add program to implement delegate as a callback

---
+++ Delegates/DelegateCallback.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.Delegates
+{
+    /*
+    Callback using Delegate
+Simulate a file download method that accepts a delegate as a callback to notify when download completes. 
+     */
+    public delegate void DownloadCallback(string message);
+    internal class DelegateCallback
+    {
+        public static void DownloadFile(DownloadCallback callback)
+        {
+            Console.WriteLine("Downloading file...");
+            Thread.Sleep(2000);
+            callback("Download completed successfully!");
+        }
+        // callback method
+        public static void OnDownloadCompleted(String msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
