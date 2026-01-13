d90cf5c4e33316280151927b02bb22a5255aa35a
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-13 14:51:42 +0530

    [Prashant] add program to implement delegate as method parameter

---
+++ Delegates/DelegateAsParameter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.Delegates
+{
+    /*
+     Delegate as Method Parameter
+Write a method PerformOperation(int a, int b, DelegateName op)
+Pass different operations using delegates.
+     */
+    public delegate void MyDelegate(string msg);
+    internal class DelegateAsParameter
+    {
+        public static void InvokeDelegate(MyDelegate del)
+        {
+            del("Hello world");
+        }
+        public static void Greet(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
