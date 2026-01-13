18c39df8e4ba8652430fa522e1fdeac2d8049926
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-13 14:52:18 +0530

    [Prashant] add program to implement delegate using anonymous method

---
+++ Delegates/Anonymous.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.Delegates
+{
+    /*
+     Anonymous Method with Delegate
+Use an anonymous method to calculate the square of a number using a delegate.
+     */
+    public delegate void Printt(int value);
+    internal class Anonymous
+    {
+    }
+}
