9bd4884717dc2e3b4529dd7a7b99611274c6c059
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-13 14:51:58 +0530

    [Prashant] add program to implement multicast delegate

---
+++ Delegates/MulticastDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.Delegates
+{
+    /*
+     Multicast Delegate
+Create a delegate that returns void.
+Attach multiple methods and observe the execution order.
+     */
+
+    public delegate void MyDel();
+    internal class MulticastDelegate
+    {
+        public static void MethodA()
+        {
+            Console.WriteLine("Method A");
+        }
+
+        public static void MethodB()
+        {
+            Console.WriteLine("Method B");
+        }
+    }
+}
