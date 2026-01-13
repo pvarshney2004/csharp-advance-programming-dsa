e4b4b0c0df5f8516f693a14c29eed570f8e15629
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-13 14:53:32 +0530

    [Prashant] add program to implement delegate chainging

---
+++ Delegates/DelegateChaining.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.Delegates
+{
+    /*
+     Delegate Chaining & Removal
+Add multiple methods to a delegate and remove one method at runtime.
+     */
+    public delegate void Notify();
+    internal class DelegateChaining
+    {
+        public static void Execute()
+        {
+            Notify notify = SMS;
+            notify += Email;
+            notify += Push;
+
+            notify();
+
+            Console.WriteLine("After removing Email:");
+            notify -= Email;
+            notify();
+        }
+
+        public static void SMS()
+        {
+            Console.WriteLine("SMS Sent");
+        }
+        public static void Email()
+        {
+            Console.WriteLine("Email Sent");
+        }
+        public static void Push()
+        {
+            Console.WriteLine("Push Notification Sent");
+        }
+    }
+}
