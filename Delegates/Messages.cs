d5f52099052409daa23a1296431cd1ff070e6b69
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-13 14:51:13 +0530

    [Prashant] add program to implement delegate with void return type

---
+++ Delegates/Messages.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advance_Programming_DSA.Delegates
+{
+    /*
+     Delegate with void Return Type
+Define a delegate that prints a message.
+Pass different methods to display:
+
+Welcome message
+Error message
+Success message
+     */
+    public delegate void Print(string msg);
+    internal class Messages
+    {
+        public static void WelcomeMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public static void ErrorMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+        public static void SuccessMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
